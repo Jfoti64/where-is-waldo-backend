8160cae58a4d2e178a6997f3069e5ab5562aa27c
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-05-31 20:10:12 -0500

    Add middleware for production

--- app.js
+++ app.js
@@ -5,12 +5,32 @@ const cors = require('cors');
 const characterRoutes = require('./routes/characterRoutes');
 const scoreRoutes = require('./routes/scoreRoutes');
 require('dotenv').config();
+const compression = require("compression");
+const helmet = require("helmet");
 
 const app = express();
 
+// Set up rate limiter: maximum of 40 requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 40,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
 // Middleware
+app.use(compression()); // Compress all routes
 app.use(cors());
 app.use(express.json());
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
 
 // Routes
 app.use('/api/characters', characterRoutes);
